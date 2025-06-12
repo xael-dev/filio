f8f49720fb0102e6ebf5e487182f69abd7cbfbf4
xael-dev <alphalimaepsilonxray@protonmail.com>
2025-06-12 09:11:43 -0400

    Import FileUtils as package

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,29 @@
-//Import the file class and NIO java file packages
-import java.io.File;
+import com.xael.filio.utils.FileUtils;
+
+import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
-//Error handling
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.StandardCopyOption;
-
-//Create a file (on UNIX based systems)
-//File myFileObj = new File("/Users/xael/Desktop/TestFolder/filetest.txt");
-
-//Basic File creation
 public class Main {
     public static void main(String[] args) throws IOException {
-//        File myObj = new File("/Users/xael/Desktop/TestFolder/filetest.txt");
-//        myObj.createNewFile();
-//        System.out.println(myObj.getName());
+        String currentWorkingDir = System.getProperty("user.dir");
+        String homeDir = System.getProperty("user.home");
 
-        System.out.println(System.getProperty("user.dir"));
+        //List system properties
+        System.out.println(System.getProperties());
+
+        System.out.println(currentWorkingDir + " is the current directory for the project. \n");
+        System.out.println(System.getProperty("user.home") + " is the home directory for the project. \n");
+
 
         try {
-            FileWriter myFileWriter = new FileWriter("/Users/xael/Desktop/TestFolder/filetest.txt");
-            myFileWriter.write("Testing write to a file created using Java");
-            myFileWriter.close();
-            System.out.println("Written to file!");
-        } catch (Exception e){
-            e.printStackTrace();
-        };
+            FileUtils.listFilesInDirectory(Paths.get(homeDir + "/Downloads/"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-        //Write to file specified, for now to file created
-//        FileWriter myFileWriter = new FileWriter("/Users/xael/Desktop/TestFolder/filetest.txt");
-//        myFileWriter.write("Testing write to a file created using Java");
-//        myFileWriter.close();
-//        System.out.println("Wrote to file");
 
-        //After writing to file move file to desktop as a test
-        Path sourcePath = Paths.get("/Users/xael/Desktop/TestFolder/filetest.txt");
-        Path targetPath = Paths.get("/Users/xael/Desktop/TestFolder2/filetest.txt");
 
-        Path path = Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
-        System.out.println(targetPath + " file copied!");
+
     }
 }
---
+++ src/com/xael/filio/utils/FileUtils.java
@@ -0,0 +1,64 @@
+package com.xael.filio;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+
+public class FileUtils {
+
+    public static boolean doesFileExist(String targetFilePath){
+        return Files.exists(Paths.get(targetFilePath));
+    }
+
+    //If file is smaller then 2GB
+    public static String readSmallFile(String targetFilePath) throws IOException {
+        return Arrays.toString(Files.readAllBytes(Paths.get(targetFilePath)));
+    }
+
+    //Consider method overloading to create and write?
+    public static String createFile(String filePath) throws IOException{
+        try {
+            File myFileObj = new File(filePath);
+            myFileObj.createNewFile();
+        } catch (Exception e){
+            e.printStackTrace(); //TODO: Replace with better logging
+        }
+        return "File created successfully";
+    }
+
+    public static void writeFile(String targetFilePath, String writeContent) throws IOException {
+        try {
+            FileWriter myFileWriter = new FileWriter(targetFilePath);
+            myFileWriter.write(writeContent);
+            myFileWriter.close();
+            System.out.println("File written!");
+        } catch (Exception e){
+            e.printStackTrace(); // TODO: Replace with better logging
+        }
+    }
+
+    public static String copyFile(Path sourcePath, Path targetPath, String optionForMove) throws IOException{
+        try {
+            Files.copy(sourcePath, targetPath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return "File Copied successfully!";
+    }
+
+    public static String moveFile(Path sourcePath, Path targetPath) throws IOException {
+        try {
+            Files.move(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            e.printStackTrace(); //TODO: Replace with better logging
+        }
+
+        return "File was moved successfully!";
+    }
+}
